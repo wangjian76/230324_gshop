4541a97db3af30599d5d69061437a7abfd553d97
wangjian_a <564116520@qq.com>
2023-03-24 14:20:50 +0800

    init gshop

---
+++ src/api/ajax.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import {reject} from "eslint-plugin-promise/rules/lib/promise-statics";
+
+export default function (url, data = {}, type = 'GET') {
+  return new Promise(function (resolve, reject) {
+    //执行异步ajax请求
+    let promise
+    if (type === 'GET') {
+      let dataStr = ''
+      Object.keys(data).forEach(key => {
+        dataStr += key + '=' + data[key] + '&'
+      })
+      if (dataStr !== '') {
+        dataStr = dataStr.substring(0, dataStr.lastIndexOf('&'))
+        url = url + '?' + dataStr
+      }
+      //发送get请求
+      promise = axios.get(url)
+    } else {
+      promise = axios.post(url, data)
+    }
+    promise.then(response => {
+      resolve(response.data)
+    }).catch(error => {
+      reject(error)
+    })
+  })
+}
---
+++ src/components/HeaderTop/vueTemplate.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "vueTemplate"
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
